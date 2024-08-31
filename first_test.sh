ef848ab09d7ebe8a0adc1ff6e548e0fad1532ef9
jonathan joly <41478399+joly534@users.noreply.github.com>
2024-08-31 14:10:18 +0200

    Update first_test.sh

--- first_test.sh
+++ first_test.sh
@@ -120,7 +120,7 @@ echo "" > test_errors.txt
 echo "Les 'resultats // results' 'se trouvent // will be written' dans le 'fichier - in file' test_errors.txt"
 for ((i=0;i<96;i++));
 do
-    echo "Résultat pour l'algorithme :${i} - ${algs[$i]}" >> test_errors.txt
+    echo "Résultat pour l'algorithme :${i} - $algs[$i]" >> test_errors.txt
     echo -e "${i}\n[100,100]\n1\n5\no" | bash scripts/test_algorithm.sh | sed -n "/#instances: 5/,/-/p" >> test_errors.txt
 done
 
